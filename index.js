01063f6f49afae3cf423d9c98e67cd2e785af5f6
lynchburg-college <hudson_b@hudson-b.(none)>
2014-07-14 15:58:09 -0400

    modified:   index.js

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ var interface_media =  {
         "read" : function() {
 
            var response=Data.send( { command:'vlm', item:'show media' } );
-           
+console.log(response);           
            if( response.result.indexOf('0 broadcast') ) {
               return;
            };
